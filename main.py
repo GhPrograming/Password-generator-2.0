8a13a87662d011f639ccd6a4427b256a579f46a5
GH cyber <89rx77@gmail.com>
2026-05-23 20:22:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,17 @@ import customtkinter
 import string
 
 X = string.ascii_letters + string.digits + string.punctuation
-#print(string.ascii_letters)
-#printeaza caractere de la "a" la "z" si de la "A" la "Z" in ordine alfabetica
-#print(string.digits)
-#numere de la 0 la 9
-#print(string.punctuation)
-# punctuatii de la ! la ~ (se iau dupa codul ascii)
-# exemplu:  ! are codul 33(minim), iar ~ are codul 126(maxim)
 
 def buton_callback():
     parola = ""
     for _ in range(10):
         parola = parola + secrets.choice(X)
-    label_parola.configure(text=parola)  # afiseaza parola in label
+    label_parola.configure(text=parola) 
 
 app = customtkinter.CTk()
 app.geometry("700x400")
-#setez dimensiunea aplicatiei
+
 customtkinter.set_appearance_mode("dark")
-#culoarea aplicatiei
 label_parola = customtkinter.CTkLabel(app, text="Aici apare parola")
 label_parola.place(x=350, y=150, anchor="center")
 
